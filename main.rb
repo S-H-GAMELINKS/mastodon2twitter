1d600bf156dabad0fbae68e05105017a53ea7ec6
S-H-GAMELINKS <gamelinks007@gmail.com>
2019-10-12 22:30:09 +0900

    add tweet

--- main.rb
+++ main.rb
@@ -1,7 +1,15 @@
 require 'dotenv'
 require 'mastodon'
+require 'twitter'
 
 Dotenv.load
+
+client = Twitter::REST::Client.new do |config|
+  config.consumer_key        = ENV["TWITTER_COMSUMER_KEY"]
+  config.consumer_secret     = ENV["TWITTER_COMSUMER_SECRET"]
+  config.access_token        = ENV["TWITTER_ACCESS_TOKEN"]
+  config.access_token_secret = ENV["TWITTER_ACCESS_SECRET"]
+end
 
 stream = Mastodon::Streaming::Client.new(base_url: ENV["MASTODON_URL"], bearer_token: ENV["MASTODON_ACCESS_TOKEN"])
 
@@ -9,6 +17,7 @@ while true
     begin
         stream.user(){|toot|
             puts toot.content
+            client.update(toot.content)
         }
     rescue => error
         puts error
